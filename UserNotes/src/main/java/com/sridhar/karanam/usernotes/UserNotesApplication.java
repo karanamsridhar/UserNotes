678c3d2cfc780e225e38d9dd4fa9c9468cb50cd5
sridhar <karanamsridhar@gmail.com>
2017-05-10 05:21:18 -0500

    Initial commit of user notes

---
+++ UserNotes/src/main/java/com/sridhar/karanam/usernotes/UserNotesApplication.java
@@ -0,0 +1,12 @@
+package com.sridhar.karanam.usernotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotesApplication.class, args);
+	}
+}
---
+++ UserNotes/src/main/java/com/sridhar/karanam/usernotes/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.sridhar.karanam.usernotes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String home(){
+		return "home";
+	}
+}
